520f9a51b19c93f749ee86e23161d946df8628fa
Lira Guedes <liracrg@gmail.com>
2023-12-08 11:53:23 -0300

    feat: req 7 - metade

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,22 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    exist = []
+
+    for i in range(0, len(instance)):
+        file = instance.search(i)
+        data = {
+            'palavra': word,
+            'arquivo': file['nome_do_arquivo'],
+            'ocorrencias': list()
+        }
+
+        for line, content in enumerate(file['linhas_do_arquivo']):
+            if word.casefold() in content.casefold():
+                data['ocorrencias'].append({'linha': line + 1})
+
+        if len(data['ocorrencias']) > 0:
+            exist.append(data)
+
+    return exist
 
 
 def search_by_word(word, instance):
