c02aff3717924f961a783608d55d09acbe3d12ab
Lira Guedes <liracrg@gmail.com>
2023-12-08 10:42:52 -0300

    req: 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file = False
+
+    for file in instance.data:
+        if file["nome_do_arquivo"] == path_file:
+            file = True
+            break
+
+    if not file:
+        lines = txt_importer(path_file)
+        dict_file = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(lines),
+            "linhas_do_arquivo": lines,
+        }
+        instance.enqueue(dict_file)
+        print(dict_file)
 
 
 def remove(instance):
